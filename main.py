dbdd2ae3e1e741051775953de1b097b0b53f01b2
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-22 16:34:47 -0600

    Ciclo de Request y Response

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 # creamos instancia de Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
+def index():
+    # request.remote_addr ==> obtiene la ip del usuario(quien realiza la peticion)
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    #response.set_cookie('user_ip', user_ip) ===> Almacena la ip en la cookie del navegador
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route("/hello")
 def hello():
-    #request.remote_addr ==> obtiene la ip del usuario(quien realiza la peticion)
-    user_ip=request.remote_addr
+    # request.cookies.get('user_ip') ==> obtiene la ip del usuario que esta almacenada en la cookie
+    user_ip = request.cookies.get('user_ip')
     return 'Bienvenido, tu IP es {}'.format(user_ip)
