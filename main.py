518bad4732168684efb25971f2e29efd319be13b
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-22 17:27:54 -0600

    Estructuras de control

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request, make_response, redirect, render_template
 # creamos instancia de Flask
 app = Flask(__name__)
 
+todos = ['Todo 1', 'Todo 2', 'Todo 3']
+
 
 @app.route('/')
 def index():
@@ -20,5 +22,12 @@ def index():
 def hello():
     # request.cookies.get('user_ip') ==> obtiene la ip del usuario que esta almacenada en la cookie
     user_ip = request.cookies.get('user_ip')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+
     # return 'Bienvenido, tu IP es {}'.format(user_ip)
-    return render_template("index.html", user_ip=user_ip)
+    # return render_template("index.html", user_ip=user_ip, todos=todos)
+    return render_template("index.html", **context)
