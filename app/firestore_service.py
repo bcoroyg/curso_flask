a63023288ec35c5a9026dcc13ce74320bc85032d
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-23 18:51:03 -0600

    signup

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -5,16 +5,25 @@ initialize_app(credential)
 
 db = firestore.client()
 
+
 def get_users():
     users = db.collection('users').get()
     return users
 
+
 def get_user(user_id):
     users = db.collection('users').document(user_id).get()
     return users
+
 
 def get_todos(user_id):
     todos = db.collection('users')\
         .document(user_id)\
         .collection('todos').get()
     return todos
+
+
+def user_put(user_data):
+    user_ref = db.collection('users')\
+                 .document(user_data.username)
+    user_ref.set({'password': user_data.password})
