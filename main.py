89c9b861d6ea48d5194ce6d23d6bb18f0106e099
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-23 21:33:00 -0600

    Agregar tareas

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask import request, make_response, redirect, render_template, session, flash, url_for
 from flask_login import login_required, current_user
 
 import unittest
 
 from app import create_app
 
-from app.firestore_service import get_todos
+from app.firestore_service import get_todos, put_todo
+
+from app.forms import TodoForm
 
 # creamos instancia de Flask
 # app = Flask(__name__)
@@ -52,7 +54,7 @@ def index():
     return response
 
 
-@app.route("/hello", methods=['GET'])
+@app.route("/hello", methods=['GET', 'POST'])
 @login_required
 def hello():
     # request.cookies.get('user_ip') ==> obtiene la ip del usuario que esta almacenada en la cookie
@@ -61,11 +63,21 @@ def hello():
     # username = session.get('username')
     username = current_user.id
 
+    todo_form = TodoForm()
+
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form
     }
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+        
+        flash('Tu tarea se creo con éxito!')
+        
+        return redirect(url_for('hello'))
 
     # return 'Bienvenido, tu IP es {}'.format(user_ip)
     # return render_template("index.html", user_ip=user_ip, todos=todos)
